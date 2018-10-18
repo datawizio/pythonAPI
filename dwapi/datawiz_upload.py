efc88c247c41ec0d90b9129c74dae78991c1c172
NikitaZakharov <nekit0990507392>
2018-10-18 17:42:36 +0300

    fixed compatibility for python3 with xrange

--- dwapi/datawiz_upload.py
+++ dwapi/datawiz_upload.py
@@ -5,6 +5,7 @@ from .datawiz_auth import Auth, APIGetError, APIUploadError
 import pandas
 import os
 import math
+import six
 # Потрібно перезавантажити модуль, щоб лог працював в ipython notebook
 # Це звязано з тим, що logging.basicConfig звертається до того ж
 # StreamHandler, що і notebook
@@ -51,6 +52,10 @@ PEOPLE_TRAFFIC_URL = 'people-traffic'
 RECEIPTS_CHUNK_SIZE = 2000
 DEFAULT_CHUNK_SIZE = 2000
 SEPARATOR = ';'
+
+
+if six.PY3:
+    xrange = range
 
 
 class Up_DW(Auth):
